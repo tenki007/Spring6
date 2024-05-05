5104e2d616c464f6c02c5abb75ae195bc7e19ec3
hash_tenki <1759276999@qq.com>
2024-05-05 22:39:05 +0800

    更改

--- spring-jdbc-tx/src/main/java/com/hashtenki/spring6/tx/service/BookServiceImpl.java
+++ spring-jdbc-tx/src/main/java/com/hashtenki/spring6/tx/service/BookServiceImpl.java
@@ -3,6 +3,7 @@ package com.hashtenki.spring6.tx.service;
 import com.hashtenki.spring6.tx.dao.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookServiceImpl implements BookService{
@@ -10,6 +11,7 @@ public class BookServiceImpl implements BookService{
     @Autowired
     private BookDao bookDao;
 
+    @Transactional()
     @Override
     public void buyBook(Integer userId, Integer bookId) {
         //根据图书id查询图书价格
